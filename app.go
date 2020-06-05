d8f007c5dc9a96b565b5e63045704cbd8078996b
robinfang <thefmajia@gmail.com>
2020-06-05 14:27:23 +0800

    等待worker执行完后再统一写文件

--- app.go
+++ app.go
@@ -5,21 +5,22 @@ import (
     "bytes"
     "fmt"
     "io/ioutil"
+    "log"
     "net/http"
     "os"
     "strings"
+    "sync"
     "time"
-    "log"
 )
 
-type Body struct {
+type bodyType struct {
     id          string
     transaction string
 }
-type Result struct {
-    id        string
-    start     int64
-    resp_time int64
+type resultType struct {
+    id       string
+    start    int64
+    respTime int64
 }
 
 func check(e error) {
@@ -32,67 +33,88 @@ var httpClient *http.Client
 
 func init() {
     httpClient = &http.Client{
-        Timeout: 0 * time.Second,
+        Timeout: 10 * time.Second,
     }
 }
 
 func main() {
+
+    var wg sync.WaitGroup
     content, err := ioutil.ReadFile("../payload_list.txt")
     check(err)
-    raw_lines := strings.Split(strings.Replace(string(content), "\r\n", "\n", -1), "\n")
-    lines := make([]Body, len(raw_lines))
-    for i, el := range raw_lines {
-        lines[i] = Body{strings.Split(el, ",")[0], strings.Split(el, ",")[1]}
+    rawLines := strings.Split(strings.Replace(string(content), "\r\n", "\n", -1), "\n")
+    fmt.Println(len(rawLines))
+    lines := make([]bodyType, 0)
+    // fmt.Printf("len lines %v, cap=%d \n", len(lines), cap(lines))
+    for _, el := range rawLines {
+        if len(el) > 0 {
+            lines = append(lines, bodyType{strings.Split(el, ",")[0], strings.Split(el, ",")[1]})
+        }
     }
-    fmt.Println(len(lines))
-    result_chan := make(chan Result, len(raw_lines)+23)
-    body_chan := make(chan Body, len(raw_lines)+23)
-
-    for i := 0; i < 32; i++ {
-        go makeAllRequests(body_chan, result_chan)
+    fmt.Printf("len lines %v, cap=%d \n", len(lines), cap(lines))
+    resultChan := make(chan resultType, len(lines)+23)
+    bodyChan := make(chan bodyType, len(lines)+23)
+    fmt.Println("go makeAllRequests")
+    for i := 0; i < 48; i++ {
+        wg.Add(1)
+        go makeAllRequests(bodyChan, resultChan, &wg)
     }
+    fmt.Println("pushing into bodyChan")
     for _, body := range lines {
-        body_chan <- body
+        bodyChan <- body
     }
-    close(body_chan)
+    close(bodyChan)
+    // go func() {
+    //     for {
+    //         time.Sleep(1 * time.Second)
+    //         fmt.Printf("len bodychan: %v, len resultchan: %v\n", len(bodyChan), len(resultChan))
+    //     }
 
+    // }()
+    fmt.Println("waiting")
+    wg.Wait()
+    fmt.Println("close resultChan")
+    close(resultChan)
     f, err := os.Create("../go_request_result.txt")
     check(err)
     defer f.Close()
     w := bufio.NewWriter(f)
     n := 0
-    for i := 0; i < len(raw_lines); i++ {
-        r := <-result_chan
+    for r := range resultChan {
         n++
-        fmt.Println(n)
-        // fmt.Printf("%s,%d,%d\n", r.id, r.start, r.resp_time)
-        fmt.Fprintf(w, "%s,%d,%d\n", r.id, r.start, r.resp_time)
+        if n%100 == 0 {
+            fmt.Println(n)
+        }
+        // fmt.Printf("%s,%d,%d\n", r.id, r.start, r.respTime)
+        fmt.Fprintf(w, "%s,%d,%d\n", r.id, r.start, r.respTime)
     }
     w.Flush()
 
 }
 
-func makeOne(url string, body Body, result_chan chan<- Result){
-     buf := bytes.NewBuffer([]byte(body.transaction))
-        start := time.Now()
-        resp, err := httpClient.Post(url, "application/json", buf)
-        if err!=nil{
-            log.Println(err)
-        }
-        ioutil.ReadAll(resp.Body)
+func makeOne(url string, body bodyType, resultChan chan<- resultType) {
+    buf := bytes.NewBuffer([]byte(body.transaction))
+    start := time.Now()
+    resp, err := httpClient.Post(url, "application/json", buf)
+    if err != nil {
+        log.Println(err)
 
-        defer resp.Body.Close()
-        resp_time := time.Since(start)
-        
-        // fmt.Printf("Total time: %v\n", time.Since(start))
-        result_chan <- Result{body.id, start.UnixNano(), resp_time.Nanoseconds()}
+    }
+    ioutil.ReadAll(resp.Body)
+
+    defer resp.Body.Close()
+    respTime := time.Since(start)
+
+    // fmt.Printf("Total time: %v\n", time.Since(start))
+    resultChan <- resultType{body.id, start.UnixNano(), respTime.Nanoseconds()}
 
 }
 
-func makeAllRequests(body_chan <-chan Body, result_chan chan<- Result) {
+func makeAllRequests(bodyChan <-chan bodyType, resultChan chan<- resultType, wg *sync.WaitGroup) {
+    defer wg.Done()
     url := "http://192.168.2.70:8081/transaction/postTranByString"
-    for body := range body_chan {
+    for body := range bodyChan {
         // fmt.Printf("%v,%v\n", body.id, body.transaction)
-        makeOne(url, body, result_chan)
+        makeOne(url, body, resultChan)
     }
 }
