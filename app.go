948a45b731cb57287aacbe81424840a90c8d0c76
robinfang <thefmajia@gmail.com>
2020-06-08 17:43:57 +0800

    修改参数获取方式

--- app.go
+++ app.go
@@ -3,12 +3,12 @@ package main
 import (
     "bufio"
     "bytes"
+    "flag"
     "fmt"
     "io/ioutil"
     "log"
     "net/http"
     "os"
-    "strconv"
     "strings"
     "sync"
     "time"
@@ -39,22 +39,22 @@ func init() {
 }
 
 func main() {
-    if len(os.Args) != 3 {
-        fmt.Println("usage: app[.exe] hostAddr:port number_of_workers")
+    fmt.Println("务必检查服务器时间与本机时间是否同步！最好利用ntp服务进行同步。")
+    var hostAddrP = flag.String("h", "", "hostAddr:port")
+    var nOfW = flag.Int("n", 1, "number of workers")
+    var interval = flag.Int("i", 0, "interval in int /time.Millisecond (optional)")
+    flag.Parse()
+    fmt.Printf("hostAddr: %s\n", *hostAddrP) // For debug
+    fmt.Printf("number of workers: %d\n", *nOfW)
+    fmt.Printf("interval: %d\n", *interval)
+    if len(*hostAddrP) == 0 {
+        fmt.Println("hostAddr:port must be provided")
         os.Exit(0)
     }
-    hostAddrP := os.Args[1]
-    numberOfWorkers := os.Args[2]
-    nOfW, _ := strconv.Atoi(numberOfWorkers)
-
-    fmt.Printf("hostAddr: %s\n", hostAddrP) // For debug
-    fmt.Printf("number of workers: %s\n", numberOfWorkers)
-    fmt.Println("务必检查服务器时间与本机时间是否同步！最好利用ntp服务进行同步。")
     var wg sync.WaitGroup
     content, err := ioutil.ReadFile("../payload_list.txt")
     check(err)
     rawLines := strings.Split(strings.Replace(string(content), "\r\n", "\n", -1), "\n")
-    fmt.Println(len(rawLines))
     lines := make([]bodyType, 0)
     // fmt.Printf("len lines %v, cap=%d \n", len(lines), cap(lines))
     for _, el := range rawLines {
@@ -66,9 +66,9 @@ func main() {
     resultChan := make(chan resultType, len(lines)+23)
     bodyChan := make(chan bodyType, len(lines)+23)
     fmt.Println("go makeAllRequests")
-    for i := 0; i < nOfW; i++ {
+    for i := 0; i < *nOfW; i++ {
         wg.Add(1)
-        go makeAllRequests(hostAddrP, bodyChan, resultChan, &wg)
+        go makeAllRequests(hostAddrP, interval, bodyChan, resultChan, &wg)
     }
     fmt.Println("pushing into bodyChan")
     for _, body := range lines {
@@ -90,15 +90,17 @@ func main() {
     check(err)
     defer f.Close()
     w := bufio.NewWriter(f)
-    n := 0
+    // n := 0
+    fmt.Println("writing to file")
     for r := range resultChan {
-        n++
-        if n%100 == 0 {
-            fmt.Println(n)
-        }
+        // n++
+        // if n%100 == 0 {
+        //     fmt.Println(n)
+        // }
         // fmt.Printf("%s,%d,%d\n", r.id, r.start, r.respTime)
         fmt.Fprintf(w, "%s,%d,%d\n", r.id, r.start, r.respTime)
     }
+    fmt.Println("flushing")
     w.Flush()
 
 }
@@ -121,12 +123,14 @@ func makeOne(url string, body bodyType, resultChan chan<- resultType) {
 
 }
 
-func makeAllRequests(hostAddr string, bodyChan <-chan bodyType, resultChan chan<- resultType, wg *sync.WaitGroup) {
+func makeAllRequests(hostAddr *string, interval *int, bodyChan <-chan bodyType, resultChan chan<- resultType, wg *sync.WaitGroup) {
     defer wg.Done()
-    url := fmt.Sprintf("http://%s/transaction/postTranByString", hostAddr)
+    url := fmt.Sprintf("http://%s/transaction/postTranByString", *hostAddr)
     for body := range bodyChan {
         // fmt.Printf("%v,%v\n", body.id, body.transaction)
-        time.Sleep(10 * time.Millisecond)
+        if *interval > 0 {
+            time.Sleep((time.Duration(*interval)) * time.Millisecond)
+        }
         makeOne(url, body, resultChan)
     }
 }
